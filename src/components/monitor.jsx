2ad3a3082f7d5cde2fc21b7bd19b255df0ad21c8
kazuki19992 <kushida98@gmail.com>
2021-08-07 23:35:20 +0900

    データを自動フェッチしてくるようにした

--- src/components/monitor.jsx
+++ src/components/monitor.jsx
@@ -16,9 +16,9 @@ export default function Monitor (props) {
   const [ssid, updateSsid] = useState('--')
   const [ipAddr, updateIpAddr] = useState('--')
   const [odoCount, updateOdoCount] = useState('--')
-  // const [latestDate, updateLatestDate] = useState('--')
+  const [latestDate, updateLatestDate] = useState(<span style={{paddingLeft: 3, textDecoration: 'bold'}}>情報を取得しています……</span>)
 
-  useEffect(() => {
+  const fetchData = () => {
     axios.get(url).then((response) => {
       // 取得成功時
       console.log("取得成功", response.data)
@@ -29,10 +29,24 @@ export default function Monitor (props) {
       updateSsid(data.ssid)
       updateIpAddr(data.ipAddr)
       updateOdoCount(data.odo)
-      // updateLatestDate(data.time)
+      updateLatestDate(data.time)
     })
+  }
+
+  useEffect(() => {
+    // 初回フェッチ
+    console.log('初回フェッチ')
+    fetchData()
+
+    // 数秒おきに取得する
+    setInterval(() => {
+      console.log("取得します")
+      fetchData()
+    }, 10000)
   }, [])
   return (
+    <>
+    <p style={{padding: 5, fontSize: 10}}>最終更新:{latestDate}</p>
     <div className="monitor">
       <div className="wrapper">
         <Temp temp={temp} humidity={humidity} />
@@ -42,5 +56,6 @@ export default function Monitor (props) {
         </div>
       </div>
     </div>
+    </>
   )
 }
